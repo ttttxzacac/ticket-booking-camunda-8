9e8f81b7459c58689158899bc216580543943b2b
Elif İpek Uysal <elifipekuysal@gmail.com>
2025-03-13 21:09:11 +0100

    set env variables for cdk

--- seat-reservation-lambda/bin/.pipeline.ts
+++ seat-reservation-lambda/bin/.pipeline.ts
@@ -2,5 +2,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { SeatReservationLambdaStack } from '../lib/.pipeline-stack';
 
+const account = process.env.CDK_DEFAULT_ACCOUNT || '515966493420';
+const region = process.env.CDK_DEFAULT_REGION || 'eu-central-1';
+
 const app = new cdk.App();
-new SeatReservationLambdaStack(app, 'SeatReservationLambdaStack', { env: { account: '515966493420', region: 'eu-central-1' } });
+new SeatReservationLambdaStack(app, 'SeatReservationLambdaStack', { env: { account, region } });
--- seat-reservation-lambda/test/.pipeline.test.ts
+++ seat-reservation-lambda/test/.pipeline.test.ts
@@ -2,11 +2,14 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as Pipeline from '../lib/.pipeline-stack';
 
+const account = process.env.CDK_DEFAULT_ACCOUNT || '515966493420';
+const region = process.env.CDK_DEFAULT_REGION || 'eu-central-1';
+
 test('Lambda Created', () => {
   const app = new cdk.App();
 
   // WHEN
-  const stack = new Pipeline.SeatReservationLambdaStack(app, 'MyTestStack');
+  const stack = new Pipeline.SeatReservationLambdaStack(app, 'MyTestStack', { env: { account, region }});
 
   // THEN
   const template = Template.fromStack(stack);
