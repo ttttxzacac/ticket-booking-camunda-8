e96bf123d52fce2aa510c19b939262917ee9b61f
Elif İpek Uysal <elifipekuysal@gmail.com>
2025-03-13 19:26:22 +0100

    define accessibility only from VPC for seat reservation lambda

--- seat-reservation-lambda/bin/.pipeline.ts
+++ seat-reservation-lambda/bin/.pipeline.ts
@@ -2,5 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { SeatReservationLambdaStack } from '../lib/.pipeline-stack';
 
+const region = process.env.CDK_DEFAULT_REGION;
+
 const app = new cdk.App();
-new SeatReservationLambdaStack(app, 'SeatReservationLambdaStack', { env: { account: '515966493420', region: 'eu-north-1' } });
+new SeatReservationLambdaStack(app, 'SeatReservationLambdaStack', { env: { region } });
--- seat-reservation-lambda/lib/.pipeline-stack.ts
+++ seat-reservation-lambda/lib/.pipeline-stack.ts
@@ -2,11 +2,24 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as path from 'path';
 
 export class SeatReservationLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(this, 'ImportedVPC', {
+      isDefault: true,
+      vpcId: 'vpc-0316cf747ad31953a',
+    });
+
+    const vpcEndpoint = new ec2.InterfaceVpcEndpoint(this, 'ApiGatewayVpcEndpoint', {
+      vpc,
+      service: ec2.InterfaceVpcEndpointAwsService.APIGATEWAY,
+      privateDnsEnabled: true, // Ensures API Gateway resolves to the private IP inside AWS
+    });
 
     const fn = new lambda.Function(this, 'SeatReservationLambda', {
       functionName: 'seat-reservation-lambda',
@@ -21,6 +34,25 @@ export class SeatReservationLambdaStack extends cdk.Stack {
       deployOptions: {
         stageName: 'v1',
       },
+      endpointConfiguration: {
+        types: [apigw.EndpointType.PRIVATE],
+        vpcEndpoints: [vpcEndpoint],
+      },
+      policy: new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            effect: iam.Effect.DENY,
+            principals: [new iam.AnyPrincipal()],
+            actions: ['execute-api:Invoke'],
+            resources: ['*'],
+            conditions: {
+              'StringNotEquals': {
+                'aws:SourceVpc': vpc.vpcId,
+              },
+            },
+          }),
+        ],
+      }),
     });
 
     const reservationResource = api.root.addResource('seat-reservation');
@@ -28,7 +60,7 @@ export class SeatReservationLambdaStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, 'ApiGatewayUrlOutput', {
       value: api.url,
-      description: 'The endpoint for the API Gateway triggering the Lambda function',
+      description: 'The endpoint for the API Gateway triggering the Lambda function (only accessible within VPC)',
       exportName: 'SeatReservationRestApiUrl'
     });
   }
