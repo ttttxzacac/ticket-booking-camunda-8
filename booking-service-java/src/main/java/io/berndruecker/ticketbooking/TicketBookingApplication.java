8e8c130f4876c7da1fae617ab9c8dccbab8e1d72
tianshenxue <tianshenxue@tencent.com>
2025-03-13 20:33:35 +0100

    fix bean

--- booking-service-java/src/main/java/io/berndruecker/ticketbooking/TicketBookingApplication.java
+++ booking-service-java/src/main/java/io/berndruecker/ticketbooking/TicketBookingApplication.java
@@ -3,6 +3,7 @@ package io.berndruecker.ticketbooking;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
 import software.amazon.awssdk.services.sqs.SqsClient;
-
+import software.amazon.awssdk.regions.Region;
 @SpringBootApplication
 @EnableZeebeClient
 @Deployment(resources = { "classpath:ticket-booking.bpmn" })
@@ -33,8 +34,10 @@ public class TicketBookingApplication {
   }
 
   @Bean
-  public SqsClient sqsClient() {
-    return SqsClient.create();
+  public SqsClient sqsClient(@Value("${aws.region:eu-central-1}") String awsRegion) {
+    return SqsClient.builder()
+            .region(Region.of(awsRegion))
+            .build();
   }
 
 }
