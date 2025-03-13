58bccc4d42494708bfa7ccacf323b09ca0fa2bf5
Elif İpek Uysal <elifipekuysal@gmail.com>
2025-03-13 20:57:42 +0100

    get region as env

--- seat-reservation-lambda/bin/.pipeline.ts
+++ seat-reservation-lambda/bin/.pipeline.ts
@@ -2,7 +2,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { SeatReservationLambdaStack } from '../lib/.pipeline-stack';
 
-const region = process.env.CDK_DEFAULT_REGION || 'eu-central-1';
-
 const app = new cdk.App();
-new SeatReservationLambdaStack(app, 'SeatReservationLambdaStack', { env: { account: '515966493420', region } });
+new SeatReservationLambdaStack(app, 'SeatReservationLambdaStack', { env: { account: '515966493420', region: 'eu-central-1' } });
