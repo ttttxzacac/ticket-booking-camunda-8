d79c1ac3566632769b30877cc9b8f3e0a0a16416
tianshenxue <tianshenxue@tencent.com>
2025-03-14 10:35:15 +0100

    test zebee   @Qualifier("zeebeClientLifecycle") 2

--- booking-service-java/src/main/java/io/berndruecker/ticketbooking/TicketBookingApplication.java
+++ booking-service-java/src/main/java/io/berndruecker/ticketbooking/TicketBookingApplication.java
@@ -40,4 +40,13 @@ public class TicketBookingApplication {
             .build();
   }
 
+  @Bean
+  public ZeebeClient zeebeClient() {
+    return ZeebeClient.newClientBuilder()
+            .gatewayAddress("zeebe-service.ticketbooking.local:26500")
+            .usePlaintext()
+            .build();
+  }
+
+
 }
