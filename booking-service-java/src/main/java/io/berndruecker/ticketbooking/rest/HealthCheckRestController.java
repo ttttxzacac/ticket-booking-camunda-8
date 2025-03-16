f8acaac73bca1cffcd441ad6b4a2558f9e833371
Xiangyu Gao <gaoxiangyu2000@gmail.com>
2025-03-16 14:09:04 +0100

    Create HealthCheckRestController.java

---
+++ booking-service-java/src/main/java/io/berndruecker/ticketbooking/rest/HealthCheckRestController.java
@@ -0,0 +1,18 @@
+package io.berndruecker.ticketbooking.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Healthy");
+    }
+}
+
+
