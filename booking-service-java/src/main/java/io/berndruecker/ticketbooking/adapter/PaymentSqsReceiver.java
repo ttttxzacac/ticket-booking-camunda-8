db69123f7fb7487008d95017365f18bb7a049caa
Xiangyu Gao <gaoxiangyu2000@gmail.com>
2025-03-18 00:48:52 +0100

    Update PaymentSqsReceiver.java

--- booking-service-java/src/main/java/io/berndruecker/ticketbooking/adapter/PaymentSqsReceiver.java
+++ booking-service-java/src/main/java/io/berndruecker/ticketbooking/adapter/PaymentSqsReceiver.java
@@ -19,11 +19,6 @@ import software.amazon.awssdk.services.sqs.model.Message;
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
 
-// //batch delete
-// import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequestEntry;
-// import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequest;
-// import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchResult;
-
 import io.camunda.zeebe.client.ZeebeClient;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -53,12 +48,9 @@ public class PaymentSqsReceiver {
     ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
             .queueUrl(paymentResponseQueueUrl)
             .maxNumberOfMessages(4)
-            .waitTimeSeconds(20)
+            .waitTimeSeconds(10)
             .build();
     List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
-
-    // // List to store batch delete requests
-    // List<DeleteMessageBatchRequestEntry> deleteRequests = new ArrayList<>();
     
     for (Message message : messages) {
       try {
@@ -71,21 +63,21 @@ public class PaymentSqsReceiver {
         // PaymentResponseMessage paymentResponse = objectMapper.readValue(message.body(), PaymentResponseMessage.class);
         logger.info("Received: " + paymentResponse);
 
-        // 3. Send a message to Zeebe Workflow
-        // client.newPublishMessageCommand()
-        //         .messageName("msg-payment-received")
-        //         .correlationKey(paymentResponse.paymentRequestId)
-        //         .variables(Collections.singletonMap("paymentConfirmationId", paymentResponse.paymentConfirmationId))
-        //         .send()
-        //         .join();
-        
-        // 可以改成异步的
+        3. Send a message to Zeebe Workflow
         client.newPublishMessageCommand()
                 .messageName("msg-payment-received")
                 .correlationKey(paymentResponse.paymentRequestId)
                 .variables(Collections.singletonMap("paymentConfirmationId", paymentResponse.paymentConfirmationId))
                 .send()
-                .thenRun(() -> logger.info("Message sent successfully"));
+                .join();
+        
+        // // 可以改成异步的
+        // client.newPublishMessageCommand()
+        //         .messageName("msg-payment-received")
+        //         .correlationKey(paymentResponse.paymentRequestId)
+        //         .variables(Collections.singletonMap("paymentConfirmationId", paymentResponse.paymentConfirmationId))
+        //         .send()
+        //         .thenRun(() -> logger.info("Message sent successfully"));
 
         // 4. Delete the SQS message after successful processing
         sqsClient.deleteMessage(DeleteMessageRequest.builder()
@@ -93,34 +85,10 @@ public class PaymentSqsReceiver {
                 .receiptHandle(message.receiptHandle())
                 .build());
 
-        // // 4. Add the message to the batch delete list
-        // deleteRequests.add(DeleteMessageBatchRequestEntry.builder()
-        //         .id(message.messageId())  // Unique identifier for the message
-        //         .receiptHandle(message.receiptHandle())
-        //         .build());
-
       } catch (Exception e) {
         logger.error("Error processing SQS message", e);
       }
     }
-    // // Perform batch delete if there are any messages to delete
-    // if (!deleteRequests.isEmpty()) {
-    //   try {
-    //     DeleteMessageBatchRequest batchRequest = DeleteMessageBatchRequest.builder()
-    //             .queueUrl(paymentResponseQueueUrl)
-    //             .entries(deleteRequests)
-    //             .build();
-    //     DeleteMessageBatchResult result = sqsClient.deleteMessageBatch(batchRequest);
-    //     if (!result.failed().isEmpty()) {
-    //       logger.error("Failed to delete some messages: " + result.failed());
-    //     } else {
-    //       logger.info("Successfully deleted messages from SQS.");
-    //     }
-    //   } catch (Exception e) {
-    //     logger.error("Error performing batch delete", e);
-    //   }
-    // }
-    
   }
 
   public static class PaymentResponseMessage {
